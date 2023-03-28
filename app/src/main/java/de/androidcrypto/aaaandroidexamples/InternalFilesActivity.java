c7c7d41d286a806a5ba850db9d5f2482d278f0ab
michaelfehr <michael_fehrgmx.de>
2023-03-28 23:24:19 +0200

    update

--- app/src/main/java/de/androidcrypto/aaaandroidexamples/InternalFilesActivity.java
+++ app/src/main/java/de/androidcrypto/aaaandroidexamples/InternalFilesActivity.java
@@ -1,9 +1,7 @@
 package de.androidcrypto.aaaandroidexamples;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,18 +11,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-    private final String TAG = "MainAct";
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13;
-    TextView tv1;
+public class InternalFilesActivity extends AppCompatActivity {
+
+    private final String TAG = "InternalFilesAct";
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7;
+    TextView tv1, tv2;
     EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_internal_files);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(myToolbar);
@@ -36,121 +46,148 @@ public class MainActivity extends AppCompatActivity {
         btn5 = findViewById(R.id.btn5);
         btn6 = findViewById(R.id.btn6);
         btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
-        btn10 = findViewById(R.id.btn10);
-        btn11 = findViewById(R.id.btn11);
-        btn12 = findViewById(R.id.btn12);
-        btn13 = findViewById(R.id.btn13);
         tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
         et1 = findViewById(R.id.et1);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "btn1");
-
+                Log.i(TAG, "btn1 back to main menu");
+                Intent intent = new Intent(InternalFilesActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "btn2 internal storage");
-                Intent intent = new Intent(MainActivity.this, InternalStorageActivity.class);
-                startActivity(intent);
+                Log.i(TAG, "btn2 Load a file from resources folder");
+
+                tv2.setText("");
+                String filesName = "data.txt";
+                String resourcesTextFileRead = null;
+                // minimum API 24
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    try {
+                        resourcesTextFileRead = getResourceFileAsString(filesName);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                String outputString = "read with getResourceFileAsString:\n" + resourcesTextFileRead;
+                tv2.setText(outputString);
+
+                // see: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+                /*
+                By default, build tools like Maven, Gradle, or common Java practice will copy all files
+                from src/main/resources to the root of target/classes or build/classes. So, when we
+                try to read a file from src/main/resources, we read the file from the root of the
+                project classpath.
+                 */
+                // the stream holding the file content
+                //InputStream is = getClass().getClassLoader().getResourceAsStream("file.txt");
+
+                // for static access, uses the class name directly
+                // InputStream isStatic = InternalFilesActivity.class.getClassLoader().getResourceAsStream("file.txt");
+
             }
         });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "btn3 external storage");
-                Intent intent = new Intent(MainActivity.this, ExternalStorageActivity.class);
-                startActivity(intent);
+                Log.i(TAG, "btn3 Load a file from resources folder byte array");
+                tv2.setText("");
+                String filesName = "data.txt";
+                String resourcesTextFileRead = null;
+                resourcesTextFileRead = new String(getResourceFileAsByteArray(filesName), StandardCharsets.UTF_8);
+                String outputString = "read byte with getResourceFileAsByteArray:\n" + resourcesTextFileRead;
+                tv2.setText(outputString);
             }
         });
 
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "btn4 shared preferences");
-                Intent intent = new Intent(MainActivity.this, SharedPreferencesActivity.class);
-                startActivity(intent);
+                Log.i(TAG, "btn4");
+
             }
         });
 
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "btn5 encrypted shared preferences");
-                Intent intent = new Intent(MainActivity.this, EncryptedSharedPreferencesActivity.class);
-                startActivity(intent);
+                Log.i(TAG, "btn5");
+
             }
         });
 
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "btn6 NFC");
-                Intent intent = new Intent(MainActivity.this, NfcActivity.class);
-                startActivity(intent);
+                Log.i(TAG, "btn 6");
+
             }
         });
 
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "btn7 NFC CreditCard");
-                //Intent intent = new Intent(MainActivity.this, NfcCreditCardActivity.class);
-                //startActivity(intent);
-            }
-        });
+                Log.i(TAG, "btn 7");
 
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "btn8 Option menu");
-                Intent intent = new Intent(MainActivity.this, OptionMenuActivity.class);
-                startActivity(intent);
             }
         });
+    }
 
-        btn10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "btn10 JSON menu");
-                Intent intent = new Intent(MainActivity.this, JsonActivity.class);
-                startActivity(intent);
+    /**
+     * Reads given resource file as a string.
+     * min. API 24
+     *
+     * @param fileName path to the resource file
+     * @return the file's contents
+     * @throws IOException if read fails for any reason
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private String getResourceFileAsString(String fileName) throws IOException {
+        //ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        ClassLoader classLoader = getClass().getClassLoader();
+        //try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+        if (classLoader != null) {
+            try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+                if (is == null) return null;
+                try (InputStreamReader isr = new InputStreamReader(is);
+                     BufferedReader reader = new BufferedReader(isr)) {
+                    return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+                }
             }
-        });
+        }
+        return null;
+    }
 
-        btn11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "btn11 Internal Files");
-                Intent intent = new Intent(MainActivity.this, InternalFilesActivity.class);
-                startActivity(intent);
+    private byte[] getResourceFileAsByteArray(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        if (classLoader != null) {
+            try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+                ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+                //Create buffer
+                byte[] buffer = new byte[4096];
+                if (is == null) return null;
+                for (;;) {
+                    int nread = is.read(buffer);
+                    if (nread <= 0) {
+                        break;
+                    }
+                    byteArray.write(buffer, 0, nread);
+                }
+                return byteArray.toByteArray();
+            } catch (IOException e) {
+                //throw new RuntimeException(e);
+                return null;
             }
-        });
-
-        btn12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "btn12 Dialogs menu");
-                Intent intent = new Intent(MainActivity.this, DialogsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn13.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "btn13");
-                //Intent intent = new Intent(MainActivity.this, JsonActivity.class);
-                //startActivity(intent);
-            }
-        });
+        }
+        return null;
     }
 
 
